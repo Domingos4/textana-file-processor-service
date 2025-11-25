b0b2cc4a2b00bee221075ed2b9fb7e3bba1f7d26
Leandro Blandi <leandronblandi@gmail.com>
2025-11-25 11:40:45 -0300

    refactor: changed to gemini implementation

---
+++ src/main/java/com/lblandi/textana/fileprocessorservice/config/AiConfiguration.java
@@ -0,0 +1,13 @@
+package com.lblandi.textana.fileprocessorservice.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiConfiguration {
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return chatClientBuilder.build();
+    }
+}
